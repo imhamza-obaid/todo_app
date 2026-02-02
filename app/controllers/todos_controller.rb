f4df5e701585a0297bfcb79fe2a47e4f29e85d8a
imhamza-obaid <imhamza.obaid@gmail.com>
2026-02-02 15:58:56 +0500

    Setup CRUD for todo

---
+++ app/controllers/todos_controller.rb
@@ -0,0 +1,45 @@
+class TodosController < ApplicationController
+  before_action :set_todo, only: %i[edit update destroy]
+
+  def index
+    @todos = Todo.order(created_at: :desc)
+    @todo = Todo.new
+  end
+
+  def create
+    @todo = Todo.new(todo_params)
+
+    if @todo.save
+      redirect_to root_path
+    else
+      @todos = Todo.order(created_at: :desc)
+      render :index, status: :unprocessable_entity
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @todo.update(todo_params)
+      redirect_to root_path
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @todo.destroy
+    redirect_to root_path
+  end
+
+  private
+
+  def set_todo
+    @todo = Todo.find(params[:id])
+  end
+
+  def todo_params
+    params.require(:todo).permit(:title, :completed)
+  end
+end
